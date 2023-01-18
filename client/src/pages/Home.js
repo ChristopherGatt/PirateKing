a39f8f5ef27648048900827bce0ca586781ecb1d
ChristopherGatt <chris.gatt.3@gmail.com>
2023-01-18 14:29:36 +1100

    more changes for questions

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useQuery } from '@apollo/client'
 import { QUERY_ME, QUERY_QUESTIONS } from '../utils/queries'
@@ -8,6 +8,9 @@ const Home = () => {
   const { data } = useQuery(QUERY_ME)
   const res = useQuery(QUERY_QUESTIONS)
   // introduce a useState to track correctly answered questions and display to screen
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [selectedOption, setSelectedOption] = useState('')
+
   return (
     <div className="container">
       <h1>Welcome {Auth.loggedIn() ? data?.me.email : 'no user'}</h1>
@@ -16,10 +19,18 @@ const Home = () => {
         {Auth.loggedIn()
           ? res.data?.questions.map((q) => (
               <div>
-                <h1>{q.question}</h1>
-                <p>a: {q.answer1}</p>
-
                 {/* {/*  instead of p tag make it a button with onClick function to check correct answer or not, make interactive where correct answer changes button color/*} */}
+                <h1>{q.question}</h1>
+
+                {
+                  <button
+                    key={q.answer1}
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => {}}
+                  ></button>
+                }
+
+                <p>a: {q.answer1}</p>
 
                 <p>b: {q.answer2}</p>
                 <p>c: {q.answer3}</p>
